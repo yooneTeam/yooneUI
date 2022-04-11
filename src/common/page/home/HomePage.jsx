842479c653ee72ce69e95c95f1ee97a7cd16c634
satoshi hata <stosto826@gmail.com>
2022-04-12 00:43:01 +0900

    微調整

--- src/common/page/home/HomePage.jsx
+++ src/common/page/home/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { atom, useRecoilState } from 'recoil'
 import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core'
 import { arrayMove, SortableContext } from '@dnd-kit/sortable'
@@ -8,6 +9,7 @@ import { useIsSettingMode } from '../../hooks/useIsSetting'
 import { useHasTouchScreen } from '../../hooks/useHasTouchScreen'
 import MovableItem from './MovableItem'
 import ComponentImporter from './ComponentImporter'
+import ComponentSelector from './ComponentSelector'
 
 const small = { xs: 4, md: 3, lg: 2 }
 const medium = { xs: 12, md: 6, lg: 4 }
@@ -15,56 +17,39 @@ const medium = { xs: 12, md: 6, lg: 4 }
 const widgetListsState = atom({
   key: 'widgetLists',
   default: [
-    { id: 1, name: 'Clock', size: small },
-    { id: 2, name: 'WeatherToday', size: small },
-    { id: 3, name: 'WeatherTommorow', size: small },
-    { id: 4, name: 'Stock', size: small },
-    { id: 5, name: 'Stock', size: small },
-    { id: 6, name: 'Stock', size: small },
-    // { id: 7, name: Neko, size: medium },
-    { id: 8, name: 'Youtube', size: medium },
-    { id: 10, name: 'Rss', size: medium },
-    { id: 11, name: 'Twitter', size: medium },
-    // { id: 9, name: Memo, size: small },
-    { id: 12, name: 'Counter', size: medium },
-    // { id: 12, name: Dice, size: small },
-    { id: 13, name: 'Spotify', size: medium },
+    // { id: 1, name: 'Clock', size: small },
+    // { id: 2, name: 'WeatherToday', size: small },
+    // { id: 3, name: 'WeatherTommorow', size: small },
+    // { id: 4, name: 'Stock', size: small },
+    // { id: 5, name: 'Stock', size: small },
+    // { id: 6, name: 'Stock', size: small },
+    // // { id: 7, name: Neko, size: medium },
+    // { id: 8, name: 'Youtube', size: medium },
+    // { id: 10, name: 'Rss', size: medium },
+    // { id: 11, name: 'Twitter', size: medium },
+    // // { id: 9, name: Memo, size: small },
+    // { id: 12, name: 'Counter', size: medium },
+    // // { id: 12, name: Dice, size: small },
+    // { id: 13, name: 'Spotify', size: medium },
   ],
   effects: [localForageEffect()],
 })
 
-// const localForageEffect =
-//   () =>
-//   ({ setSelf, onSet, trigger, node }) => {
-//     const loadPersisted = async () => {
-//       const savedValue = await localforage.getItem(node.key)
-//       if (savedValue != null) setSelf(JSON.parse(savedValue))
-//     }
-
-//     if (trigger === 'get') loadPersisted()
-
-//     onSet((newValue, _, isReset) => {
-//       isReset ? localforage.removeItem(node.key) : localforage.setItem(node.key, JSON.stringify(newValue))
-//     })
-//   }
-
-// const widgetComponentState = selector({
-//   key: 'widgetComponentState',
-//   get: ({ get }) => {
-//     console.log(get(widgetListsState))
-//     return get(widgetListsState)?.reduce((sum, { name }) => {
-//       sum[name] = memo(lazy(() => import('../../../widget' + widgetPass[name])))
-//       return sum
-//     }, {})
-//   },
-// })
-
 export default function Home() {
   const [items, setItems] = useRecoilState(widgetListsState)
-
   const { hasTouchScreen } = useHasTouchScreen()
   const { isSettingMode } = useIsSettingMode()
+  const [open, setOpen] = useState(false)
+
   const sensors = useSensors(useSensor(hasTouchScreen ? TouchSensor : MouseSensor))
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (value) => {
+    setOpen(false)
+  }
 
   function handleDragEnd(event) {
     const { active, over } = event
@@ -81,6 +66,7 @@ export default function Home() {
     <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
       <SortableContext items={items}>
         <Container>
+          <ComponentSelector open={open} onClose={handleClose} />
           <Grid container spacing={1} alignItems='stretch'>
             {items.map(({ size, name, id }, index) => (
               <MovableItem key={id} id={id} size={size}>
@@ -89,8 +75,8 @@ export default function Home() {
             ))}
             {isSettingMode && (
               <Grid item {...medium} sx={{ height: 'auto', minHeight: '200px', display: 'grid', placeItems: 'center' }}>
-                <IconButton>
-                  <AddCircleIcon sx={{ width: '60px', height: '60px', color: 'primary.dark' }} />
+                <IconButton onClick={handleClickOpen}>
+                  <AddCircleIcon sx={{ width: '60px', height: '60px', color: 'primary.main' }} />
                 </IconButton>
               </Grid>
             )}
