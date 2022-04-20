61c6a6b3c3d5c9d650a6d2ad889371d7ffe2ecd2
satoshi hata <stosto826@gmail.com>
2022-04-21 00:54:45 +0900

    ストップウォッチv1.0 永続化完了

--- src/widget/clock/StopWatch.js
+++ src/widget/clock/StopWatch.js
@@ -1,24 +1,30 @@
+import { useEffect } from 'react'
 import { Stack, Typography, Button } from '@mui/material'
-import format from 'date-fns/format'
+import { format } from 'date-fns'
 import { useTheme } from '@emotion/react'
 import useStopWatch from '../../common/hooks/useStopWatch'
 
-export default function StopWatch(id) {
-  const { clickStart, clickStop, clickReset, isWatching, elapsedTime } = useStopWatch(id)
+export default function StopWatch({ id, isHidden }) {
+  const { clickStart, clickStop, clickReset, switchActiveMode, switchPassiveMode, isWatching, elapsedTime } = useStopWatch(id)
+
+  useEffect(() => {
+    isHidden ? switchPassiveMode() : switchActiveMode()
+  }, [isHidden])
 
   const handlePlay = () => {
     isWatching ? clickStop() : clickStart()
   }
 
   const formatMilliseconds = (ms) => {
-    const helperDate = new Date(ms)
-    return format(helperDate, 'mm:ss.SS')
+    const helperDate = new Date(ms + 15 * 60 * 60 * 1000)
+    const formatType = ms < 60 * 1000 ? 'ss.SS' : ms < 60 * 60 * 1000 ? 'mm:ss.SS' : 'HH:mm:ss.SS'
+    return format(helperDate, formatType)
   }
 
   return (
     <div style={{ height: '100%', width: '100%', display: 'grid', placeItems: 'center' }}>
       <Stack spacing={1} alignItems='center'>
-        <Typography fontSize='2.5rem' fontWeight='400'>
+        <Typography fontSize='3rem' fontWeight='300'>
           {formatMilliseconds(elapsedTime)}
         </Typography>
         <Stack direction='row' spacing={2}>
